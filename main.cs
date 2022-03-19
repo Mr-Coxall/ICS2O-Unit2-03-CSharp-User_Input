c206c6d694ba61b8d911dd11ffb4b5ce15ff4481
Patrick Coxall <mr.coxall@mths.ca>
2022-03-19 20:18:46 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program accepts user input
+
+using System;
+
+class Program 
+{
+    public static void Main (string[] args) {
+    	// This function does addition
+	    string firstName;
+	    int age;
+        
+        Console.WriteLine ("This program gets a user's name and age.");
+        Console.WriteLine ("");
+        
+        Console.Write("Enter your first name: ");
+        firstName = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + firstName + ", age " + age + ".");
+
+        Console.WriteLine("\nDone.");
+    }
+}
